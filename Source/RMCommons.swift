ae0c0452bb5eac553a21206b258ec35b0e727b07
Roger Molas <r.molas06@gmail.com>
2018-04-24 19:57:24 +0800

    add sample project

--- Source/RMCommons.swift
+++ Source/RMCommons.swift
@@ -209,14 +209,3 @@ extension NSNull: RMHttpProtocol {
         return NSNull.self
     }
 }
-
-func associatedObject<ValueType: AnyObject>( base: AnyObject, key: UnsafePointer<UInt8>, initialiser: () -> ValueType) -> ValueType {
-    if let associated = objc_getAssociatedObject(base, key) as? ValueType { return associated }
-    let associated = initialiser()
-    objc_setAssociatedObject(base, key, associated, .OBJC_ASSOCIATION_RETAIN)
-    return associated
-}
-
-func associateObject<ValueType: AnyObject>(base: AnyObject, key: UnsafePointer<UInt8>, value: ValueType) {
-    objc_setAssociatedObject(base, key, value,.OBJC_ASSOCIATION_RETAIN)
-}
