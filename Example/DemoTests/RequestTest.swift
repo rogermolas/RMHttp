993148927152ec4572706ae38919657911c1be58
Roger Molas <r.molas06@gmail.com>
2019-05-07 18:58:14 +0800

    Add Failure Test handling

--- Example/DemoTests/RequestTest.swift
+++ Example/DemoTests/RequestTest.swift
@@ -56,6 +56,24 @@ class RequestTest: XCTestCase {
         wait(for: [expectation], timeout: 10.0)
     }
     
+    func testPOSTJSONRequest() {
+        let expectation = XCTestExpectation(description: "testPOSTJSONRequest")
+        let urlString = "https://httpbin.org/post"
+        let request = RMRequest(urlString, method: .POST(.JSONEncoding), parameters: nil, hearders: nil)
+        RMHttp.JSON(request: request) { (response:JSONObject?, error) in
+            guard error == nil else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            if let data = response {
+                XCTAssertNotNil(data, "Expected response")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
     func testDELETERequest() {
         let expectation = XCTestExpectation(description: "testDELETERequest")
         RMHttp.isDebug = true
@@ -90,6 +108,41 @@ class RequestTest: XCTestCase {
                  XCTAssertNotNil(data, "Expected response")
             }
             expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testGETErrorRequest() { // 404
+        let expectation = XCTestExpectation(description: "testGETwithErrorsRequest_404")
+        RMHttp.isDebug = true
+        let urlString = "https://httpbin.org/get12345"
+        let request = RMRequest(urlString, method: .GET(.URLEncoding), parameters: params, hearders: nil)
+        RMHttp.JSON(request: request) { (response:JSONObject?, error) in
+            guard error == nil else {
+                XCTAssertNotNil(error, "Expected response")
+                XCTAssertNotNil(error?.localizeDescription, "Expected Error Message")
+                XCTAssertEqual(404, error?.statusCode)
+                expectation.fulfill()
+                return
+            }
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    
+    func testGETError405Request() { // 405
+        let expectation = XCTestExpectation(description: "testGETwithErrorsRequest_405")
+        RMHttp.isDebug = true
+        let urlString = "https://httpbin.org/get"
+        let request = RMRequest(urlString, method: .PUT(.URLEncoding), parameters: params, hearders: nil)
+        RMHttp.JSON(request: request) { (response:JSONObject?, error) in
+            guard error == nil else {
+                XCTAssertNotNil(error, "Expected response")
+                XCTAssertNotNil(error?.localizeDescription, "Expected Error Message")
+                XCTAssertEqual(405, error?.statusCode)
+                expectation.fulfill()
+                return
+            }
         }
         wait(for: [expectation], timeout: 10.0)
     }
--- RMHttp.podspec
+++ RMHttp.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
     s.name         = 'RMHttp'
-    s.version      = '1.3.1'
+    s.version      = '1.4'
     s.summary      = 'Lightweight RESTful library for iOS and watchOS'
 
     s.homepage         = 'https://github.com/rogermolas/RMHttp'
